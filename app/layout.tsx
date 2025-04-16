f9f7f810d905c7cd41949ce80d855667f68a3657
M-Itrych <michal.itrych1312@gmail.com>
2025-04-16 17:59:49 +0200

    small ui fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#f8f8f8]`}
       >
         <div className="relative min-h-screen flex flex-col">
           <NavBar />
