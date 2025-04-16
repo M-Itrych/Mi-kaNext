d7a21903776a24bf355978f882d5d859b86be8b9
Wizrat-KJ <gdx162454@student.gdansk.merito.pl>
2025-04-16 14:19:09 +0200

    1st commit

---
+++ componets/navigation/MainPageFooter.tsx
@@ -0,0 +1,9 @@
+const MainPageFooter = () => {
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-20 bg-white text-black">
+      <div>Kitek</div>
+    </div>
+  );
+};
+
+export default MainPageFooter;
